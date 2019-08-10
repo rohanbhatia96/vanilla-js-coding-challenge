e83ff6c5b7bb4756cabf6b64926cbce548d5f61f
Rohan <rohanbhatia96@outlook.com>
2019-08-10 07:23:46 +0530

    mk1

--- js-drum-kit/drum.js
+++ js-drum-kit/drum.js
@@ -1,5 +1,23 @@
 window.addEventListener('keydown', function(e){
 
-	console.log(e);
-	console.log(e.keyCode);
+	const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+	if(!audio)
+		return;
+	const keyDiv = document.querySelector(`div[data-key="${e.keyCode}"]`);
+	keyDiv.classList.add('playing');
+	audio.currentTime = 0;
+	audio.play();
+
+	//setTimeout(keyDiv.classList.remove('playing'),1000);
+
+});
+
+
+const keys = document.querySelectorAll(`.key`);
+
+keys.forEach(key => {
+
+	key.addEventListener('transitionend', function(e){
+		this.classList.remove('playing');
+	});
 });
